a064f4e5a2d58ca10224c292013e70448cd47551
LI Bo <li@li-HP-EliteBook-2570p.(none)>
2014-03-27 09:40:58 +0800

    added matlab code for the gauss-krueger projection

---
+++ matlab/demo.m
@@ -0,0 +1,9 @@
+f = 1/298.3;
+e = sqrt(2 * f - f^2);
+B = pi/4;
+q = geoc2isom(pi/4, e);
+omega = q + 2 * pi / 180 * 1i;
+phi = isom2geoc(omega, e);
+a = 6378245;
+z = meridianarclen(phi, e, a);
+fprintf('%.5f %.5f\n', real(z), imag(z));
---
+++ matlab/geoc2isom.m
@@ -0,0 +1,3 @@
+function q = geoc2isom(B, e)
+q = log(tan(pi/4 + B/2)) +  e/2 * log((1 - e * sin(B))/(1 + e * sin(B)));
+end
